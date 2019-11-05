bb189deda409106cbdf7194c06f72bf033bfcb8f
Student <example@example.com>
2019-11-04 16:44:01 -0800

    add a task list program

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,10 @@
 
-function ToDoList() {
+function ToDoList() {  // Contstructor function- the Blue Print
   this.toDoItems= [],
   this.currentId = 0
 }
 
-
-
-ToDoList.prototype.addToDoItem = function(newTaskObject) {
+ToDoList.prototype.addToDoItem = function(newTaskObject) { //
   newTaskObject.id = this.assignId();
   this.toDoItems.push(newTaskObject);
 }
@@ -21,35 +19,52 @@ ToDoList.prototype.assignId = function() {
 
 
 
+
+
 function TaskObject(task, person) {
   this.task = task,
   this.person = person
-
 }
 
-var toDoList = new ToDoList();
-// console.log(toDoList);
+var toDoList = new ToDoList(); // variale that stores the To Do List
+
+function displayContactDetails(toDoListToDisplay) {
+  var tasksList = $("ul#toDoItems");
+  var htmlForContactInfo = "";
+  toDoListToDisplay.toDoItem.forEach(function(newTaskObject) {
+    htmlForContactInfo += "<li id=" + newTaskObject.id + ">" + newTaskObject.task + " " + contact.person + "</li>";
+  });
+  contactsList.html(htmlForContactInfo);
+};
+
+// function displayContactDetails(addressBookToDisplay) {
+//   var contactsList = $("ul#contacts");
+//   var htmlForContactInfo = "";
+//   addressBookToDisplay.contacts.forEach(function(contact) {
+//     htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "</li>";
+//   });
+//   contactsList.html(htmlForContactInfo);
+// };
+
 
     $(document).ready(function() {
 
-
       $("form#userFormInput").submit(function(event) {
         event.preventDefault();
-
 
         var task = $("input#userInput").val();
         var person = $("input#personInput").val();
 
         var newTaskObject = new TaskObject(task, person);
 
-
         toDoList.addToDoItem(newTaskObject);
-        console.log(toDoList);
 
+displayContactDetails(toDoList);
 
+         // fires the addToDoItem prototype function to push
 
-
-
+        console.log(toDoList.toDoItems);
+// console.log(newTaskObject);
 
 
 
